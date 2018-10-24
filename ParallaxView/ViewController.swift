189bafc6e23b780294181ae7e48ffb008a9f9090
Mukesh Yadav <mails4ymukesh@gmail.com>
2018-10-24 10:22:53 +0530

    Initial Commit

---
+++ ParallaxView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParallaxView
+//
+//  Created by Mukesh on 24/10/18.
+//  Copyright © 2018 BooEat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
